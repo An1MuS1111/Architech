62619f67bff1c81df67740ce4c56ae38a50954fa
Amyzal Muhammad Zikrullah Bin Yahya <amzikrullah2@graduate.utm.my>
2024-05-01 20:35:01 +0800

    displayverification,forgot password, logout front end

---
+++ lib/logout.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // Implement logout logic here
+            Navigator.pushReplacementNamed(context, '/login');
+          },
+          child: Text('Logout'),
+        ),
+      ),
+    );
+  }
+}
