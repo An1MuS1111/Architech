60a5001dab418c2b771ab754002d15aa00b7c310
abbyspeed <wnabihah09@gmail.com>
2024-06-16 12:55:26 +0800

    added search function for pickup (incomplete)

--- lib/components/form.dart
+++ lib/components/form.dart
@@ -1,168 +1,165 @@
 import 'package:architech/config/theme.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 // Textfields
-Widget authTextFormField(String? text, String hintText, bool isPassword,
-  TextEditingController controller, Function validatorFunction) {
+Widget authTextFormField(String? text, String hintText, bool isPassword, TextEditingController controller, Function validatorFunction) {
   return Container(
     alignment: Alignment.bottomLeft,
-    child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
-      text != null ? Padding(
-        padding: const EdgeInsets.only(bottom: 8),
-        child: SizedBox(
-          width: double.infinity,
-          child: Text(
-            text!,
-            style: TextStyle(
-              fontSize: regular,
-              color: Colors.black,
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.start, 
+      children: [
+      text != null
+        ? Padding(
+            padding: const EdgeInsets.only(bottom: 8),
+            child: SizedBox(
+              width: double.infinity,
+              child: Text(
+                text!,
+                style: TextStyle(
+                  fontSize: regular,
+                  color: Colors.black,
+                ),
+              ),
             ),
-          ),
+          )
+        : const SizedBox(width: 0, height: 0),
+        TextFormField(
+          validator: (value) => validatorFunction(value),
+          controller: controller,
+          obscureText: isPassword,
+          enableSuggestions: !isPassword,
+          autocorrect: !isPassword,
+          cursorColor: primaryColour,
+          decoration: InputDecoration(
+              hintText: hintText,
+              hintStyle: const TextStyle(
+                fontWeight: FontWeight.normal,
+                color: Colors.grey,
+              ),
+              suffixIcon: isPassword
+                  ? const Padding(
+                      padding: EdgeInsets.only(right: 10),
+                      child: Icon(Icons.remove_red_eye,
+                          color: Colors.grey, size: 24),
+                    )
+                  : null,
+              isDense: true,
+              contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+              focusedBorder:
+                  const OutlineInputBorder(borderSide: BorderSide(width: 2)),
+              border: OutlineInputBorder(
+                  borderSide:
+                      const BorderSide(width: 1, style: BorderStyle.solid),
+                  borderRadius: BorderRadius.circular(4))),
+          keyboardType: isPassword
+              ? TextInputType.visiblePassword
+              : TextInputType.emailAddress,
         ),
-      ): const SizedBox(width: 0, height: 0),
-      TextFormField(
-        validator: (value) => validatorFunction(value),
-        controller: controller,
-        obscureText: isPassword,
-        enableSuggestions: !isPassword,
-        autocorrect: !isPassword,
-        cursorColor: primaryColour,
-        decoration: InputDecoration(
-          hintText: hintText,
-          hintStyle: const TextStyle(
-            fontWeight: FontWeight.normal,
-            color: Colors.grey,
-          ),
-          suffixIcon: isPassword
-              ? const Padding(
-                  padding: EdgeInsets.only(right: 10),
-                  child: Icon(Icons.remove_red_eye,
-                      color: Colors.grey, size: 24),
-                )
-              : null,
-          isDense: true,
-          contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
-          focusedBorder: const OutlineInputBorder(
-            borderSide: BorderSide(
-              width: 2
-            )
-          ),
-          border: OutlineInputBorder(
-              borderSide:
-                  const BorderSide(width: 1, style: BorderStyle.solid),
-              borderRadius: BorderRadius.circular(4))),
-        keyboardType: isPassword
-            ? TextInputType.visiblePassword
-            : TextInputType.emailAddress,
-      ),
-      const SizedBox(height: 20),
-    ]),
+        const SizedBox(height: 20),
+      ]
+    ),
   );
 }
 
-Widget textFormField(String? text, String hintText,
-  TextEditingController controller, Function validatorFunction) {
+Widget textFormField(String? text, String hintText, TextEditingController controller, Function validatorFunction, {TextInputType? keyboardType}) {
   return Container(
     alignment: Alignment.bottomLeft,
     child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
-      text != null ? Padding(
-        padding: const EdgeInsets.only(bottom: 8),
-        child: SizedBox(
-          width: double.infinity,
-          child: Text(
-            text!,
-            style: TextStyle(
-              fontSize: regular,
-              color: Colors.black,
+      text != null
+        ? Padding(
+            padding: const EdgeInsets.only(bottom: 8),
+            child: SizedBox(
+              width: double.infinity,
+              child: Text(
+                text!,
+                style: TextStyle(
+                  fontSize: regular,
+                  color: Colors.black,
+                ),
+              ),
             ),
-          ),
+          )
+        : const SizedBox(width: 0, height: 0),
+        TextFormField(
+          validator: (value) => validatorFunction(value),
+          controller: controller,
+          obscureText: false,
+          enableSuggestions: false,
+          autocorrect: true,
+          cursorColor: primaryColour,
+          decoration: InputDecoration(
+            hintText: hintText,
+            hintStyle: const TextStyle(
+              fontWeight: FontWeight.normal,
+              color: Colors.grey,
+            ),
+            isDense: true,
+            contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+            focusedBorder:
+                const OutlineInputBorder(borderSide: BorderSide(width: 2)),
+            border: OutlineInputBorder(
+                borderSide:
+                    const BorderSide(width: 1, style: BorderStyle.solid),
+                borderRadius: BorderRadius.circular(4))),
+          keyboardType: keyboardType ?? TextInputType.name,
         ),
-      ): const SizedBox(width: 0, height: 0),
-      TextFormField(
-        validator: (value) => validatorFunction(value),
-        controller: controller,
-        obscureText: false,
-        enableSuggestions: false,
-        autocorrect: true,
-        cursorColor: primaryColour,
-        decoration: InputDecoration(
-          hintText: hintText,
-          hintStyle: const TextStyle(
-            fontWeight: FontWeight.normal,
-            color: Colors.grey,
-          ),
-          isDense: true,
-          contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
-          focusedBorder: const OutlineInputBorder(
-            borderSide: BorderSide(
-              width: 2
-            )
-          ),
-          border: OutlineInputBorder(
-              borderSide:
-                  const BorderSide(width: 1, style: BorderStyle.solid),
-              borderRadius: BorderRadius.circular(4))),
-        keyboardType: TextInputType.name
-      ),
-      const SizedBox(height: 20),
-    ]),
+        const SizedBox(height: 20),
+      ]
+    ),
   );
 }
 
 // Double-sided textfield
 Widget duoTextField(String text, String hintText, bool isPassword,
-  TextEditingController controller) {
+    TextEditingController controller) {
   return Container(
-    child: Row(
-      children: [
-        Container(
-          padding: EdgeInsets.symmetric(horizontal: 15),
-          width: 140,
-          child: Text(text),
+      child: Row(
+    children: [
+      Container(
+        padding: EdgeInsets.symmetric(horizontal: 15),
+        width: 140,
+        child: Text(text),
+      ),
+      Expanded(
+        child: TextField(
+          controller: controller,
+          obscureText: isPassword,
+          enableSuggestions: !isPassword,
+          autocorrect: !isPassword,
+          cursorColor: primaryColour,
+          decoration: InputDecoration(
+              hintText: hintText,
+              hintStyle: const TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.normal,
+                color: Colors.grey,
+              ),
+              suffixIcon: isPassword
+                  ? const Padding(
+                      padding: EdgeInsets.only(right: 10),
+                      child: Icon(Icons.remove_red_eye,
+                          color: Colors.grey, size: 24),
+                    )
+                  : null,
+              isDense: true,
+              contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+              border: OutlineInputBorder(
+                  borderSide:
+                      const BorderSide(width: 1, style: BorderStyle.solid),
+                  borderRadius: BorderRadius.circular(4))),
+          keyboardType: isPassword
+              ? TextInputType.visiblePassword
+              : TextInputType.emailAddress,
         ),
-        Expanded(
-          child: TextField(
-            controller: controller,
-            obscureText: isPassword,
-            enableSuggestions: !isPassword,
-            autocorrect: !isPassword,
-            cursorColor: primaryColour,
-            decoration: InputDecoration(
-                hintText: hintText,
-                hintStyle: const TextStyle(
-                  fontSize: 14,
-                  fontWeight: FontWeight.normal,
-                  color: Colors.grey,
-                ),
-                suffixIcon: isPassword
-                    ? const Padding(
-                        padding: EdgeInsets.only(right: 10),
-                        child: Icon(Icons.remove_red_eye,
-                            color: Colors.grey, size: 24),
-                      )
-                    : null,
-                isDense: true,
-                contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
-                border: OutlineInputBorder(
-                    borderSide:
-                        const BorderSide(width: 1, style: BorderStyle.solid),
-                    borderRadius: BorderRadius.circular(4))),
-            keyboardType: isPassword
-                ? TextInputType.visiblePassword
-                : TextInputType.emailAddress,
-          ),
-        ),
-      ],
-    )
-  );
+      ),
+    ],
+  ));
 }
 
 // Clickable texts
 Widget textLink(
-  BuildContext context, String firstText, String secondText, Color colour) {
+    BuildContext context, String firstText, String secondText, Color colour) {
   return Container(
     margin: const EdgeInsets.only(top: 15),
     child: RichText(
@@ -174,8 +171,8 @@ Widget textLink(
 }
 
 // Buttons
-Widget mainBtn(
-    BuildContext context, String text, bool isLogin, Function onTap) {
+Widget mainBtn(BuildContext context, String text, bool isLogin, Function onTap,
+    {bool isLoading = false}) {
   return Container(
     width: MediaQuery.of(context).size.width * 0.92,
     height: 50,
@@ -187,10 +184,15 @@ Widget mainBtn(
           shape: MaterialStateProperty.all<RoundedRectangleBorder>(
             RoundedRectangleBorder(borderRadius: BorderRadius.circular(45)),
           )),
-      child: Text(
-        text,
-        style: const TextStyle(color: Colors.white),
-      ),
+      child: isLoading
+          ? const SpinKitWave(
+              color: Colors.white,
+              size: 25.0,
+            )
+          : Text(
+              text,
+              style: const TextStyle(color: Colors.white),
+            ),
     ),
   );
 }
@@ -201,41 +203,38 @@ Widget outlinedBtn(BuildContext context, String? text, IconData? icon, Function 
 
   if (text != null && icon != null) {
     child = OutlinedButton(
-      onPressed: (){
+      onPressed: () {
         onTap();
       },
-      style: OutlinedButton.styleFrom(
-        side: BorderSide(
-          color: primaryColour
-        )
-      ),
+      style: OutlinedButton.styleFrom(side: BorderSide(color: primaryColour)),
       child: Container(
-          width: MediaQuery.of(context).size.width,
-          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-          child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+        width: MediaQuery.of(context).size.width,
+        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center, children: [
             Text(
               text,
               textAlign: TextAlign.center,
               style: TextStyle(color: primaryColour),
             ),
             Icon(icon, size: 14, color: primaryColour),
-          ])),
+          ]
+        )
+      ),
     );
   } else if (icon == null) {
     child = OutlinedButton(
-      onPressed: (){
+      onPressed: () {
         onTap();
       },
       style: OutlinedButton.styleFrom(
-        side: BorderSide(
-          color: primaryColour
-        )
+        side: BorderSide(color: primaryColour)
       ),
       child: Container(
         width: MediaQuery.of(context).size.width,
         padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
         child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center, 
           children: [
             Text(
               text!,
@@ -246,16 +245,12 @@ Widget outlinedBtn(BuildContext context, String? text, IconData? icon, Function 
         )
       )
     );
-  }else if (text == null){
+  } else if (text == null) {
     child = OutlinedButton(
-      onPressed: (){
+      onPressed: () {
         onTap();
       },
-      style: OutlinedButton.styleFrom(
-        side: BorderSide(
-          color: primaryColour
-        )
-      ),
+      style: OutlinedButton.styleFrom(side: BorderSide(color: primaryColour)),
       child: Container(
           width: MediaQuery.of(context).size.width,
           padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
