41f5032821c992c6fe046d7f41b7c3be9405cf5e
abbyspeed <wnabihah09@gmail.com>
2024-06-19 22:27:43 +0800

    Revert "added push notification && razor payment"
    This reverts commit f238f917ac18597ae860846486ec640db3bfbc90.

--- lib/api/firebase_api.dart
+++ lib/api/firebase_api.dart
@@ -1,27 +1,4 @@
-import "package:architech/main.dart";
 import "package:firebase_messaging/firebase_messaging.dart";
-import "package:architech/pages/order/orderTracking.dart";
-
-void handleMessage(RemoteMessage? message) {
-  if (message == null) return;
-
-  navigatorKey.currentState?.pushNamed(
-    OrderTracking.route,
-    arguments: message,
-  );
-}
-
-Future initPushNotifications() async {
-  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
-    alert: true,
-    badge: true,
-    sound: true,
-  );
-
-  FirebaseMessaging.instance.getInitialMessage().then(handleMessage);
-  FirebaseMessaging.onMessageOpenedApp.listen(handleMessage);
-  FirebaseMessaging.onBackgroundMessage(handleBackgroundMessaging);
-}
 
 Future<void> handleBackgroundMessaging(RemoteMessage message) async {
   print('Title: ${message.notification?.title}');
@@ -36,7 +13,6 @@ class FirebaseApi {
     await _firebaseMessaging.requestPermission();
     final fCMToken = await _firebaseMessaging.getToken();
     print('Token: $fCMToken');
-    initPushNotifications();
     FirebaseMessaging.onBackgroundMessage(handleBackgroundMessaging);
   }
 }
